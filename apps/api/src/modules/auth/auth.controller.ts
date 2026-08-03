08084f8ed5ee08c1a63cfbcc97b62a26ad079e33
yanzhao <zhaoy@LAPTOP-R9C8FOTQ.localdomain>
2026-08-04 00:01:53 +0200

    feat(OAuth-google): implement OAuth login logic with Google Account for backend side.

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus, Get, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { type Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +18,23 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleLoginCallback(@Req() req: any, @Res() res: Response) {
+    const tokenData = await this.authService.generateToken(req.user);
+
+    return res.json({
+      message: 'Log in success with Google account!',
+      user: req.user,
+      ...tokenData,
+    })
+    // return res.redirect(`http://localhost:5173/auth/success?token=${tokenData.access_token}`);
   }
 }

af175fa7a350ddd7fbafdfcbc23692e635faad12
zhwu <zhwu@42.fr>
2026-08-08 18:05:12 +0200

    fix umsafe assignement/unsafe argument

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,7 @@ import { ALLOWED_ORIGINS } from './config/cors.config';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ZodValidationPipe(),
-  );
+  app.useGlobalPipes(new ZodValidationPipe());
 
   app.enableCors({
     origin: ALLOWED_ORIGINS,

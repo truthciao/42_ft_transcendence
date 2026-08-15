f08401436a863baaf5f3fc125ccada3c6b97f7a1
Yali <truth.ciao@gmail.com>
2026-08-15 14:06:43 +0200

    typo fix

--- apps/api/src/modules/friends/friends.module.ts
+++ apps/api/src/modules/friends/friends.module.ts
@@ -1,11 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { FriendsController } from './friends.controller.js';
 import { FriendsService } from './friends.service.js';
-import { ChatModule } from '../chat/chat.moudle.js';
-import { RealtimeMoudule } from '../realtime/realtime.module.js';
+import { ChatModule } from '../chat/chat.module.js';
+import { RealtimeModule } from '../realtime/realtime.module.js';
 
 @Module({
-  imports: [forwardRef(() => ChatModule), RealtimeMoudule,],
+  imports: [forwardRef(() => ChatModule), RealtimeModule,],
   controllers: [FriendsController],
   providers: [FriendsService],
   exports: [FriendsService],
